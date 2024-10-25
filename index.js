b437725b8a18078df5e0dbb0dcff534b2d8fb436
XENONCYBER <151541984+XENONCYBER@users.noreply.github.com>
2024-10-25 23:44:56 +0530

    changedd port

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const getHistory = require("./features/history/history");
 const getBorrowedList = require("./features/borrowed-list/borrowed-list");
 
 const app = express();
+
 const port = 8081;
+
+//middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,5 +24,5 @@ app.use("/api", getHistory);
 app.use("/api", getBorrowedList);
 
 app.listen(port, () => {
-  console.log("Server is running on http://localhost:5500");
+  console.log(`Server is running on http://localhost:${port}`);
 });
